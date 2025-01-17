ec1b9cdaf188c385beed8990ca477f77a9009e72
leasorssini <dlegasr@gmail.com>
2025-01-17 05:17:04 -0300

    expire

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_name" {
   description = "Nome do bucket S3"
-  value       = aws_s3_bucket.video_frame_pro.bucket
+  value       = aws_s3_bucket.video_frame_pro_s3.bucket
 }
 
 output "s3_bucket_arn" {
   description = "ARN do bucket S3"
-  value       = aws_s3_bucket.video_frame_pro.arn
+  value       = aws_s3_bucket.video_frame_pro_s3.arn
 }
